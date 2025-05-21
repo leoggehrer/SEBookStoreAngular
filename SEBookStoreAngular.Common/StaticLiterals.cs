2a9782ff31dc6bbf8a36dae41cf79d0bf349afb4
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2025-05-22 00:03:26 +0200

    ├änderungen in SEBookStoreAngular committen

--- SEBookStoreAngular.Common/StaticLiterals.cs
+++ SEBookStoreAngular.Common/StaticLiterals.cs
@@ -211,6 +211,7 @@ namespace SEBookStoreAngular.Common
             $".vs",
             $".vscode",
             $".git",
+            $".github",
             $"bin",
             $"obj",
             $"node_modules",
